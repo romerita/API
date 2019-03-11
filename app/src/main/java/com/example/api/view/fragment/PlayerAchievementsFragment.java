863a7c488b5077383c65658dbad28f5b1f4625fd
romerita <jruiz@elpui.xeill.net>
2019-03-11 21:03:55 +0100

    Fragments updated

--- app/src/main/java/com/example/api/view/fragment/PlayerAchievementsFragment.java
+++ app/src/main/java/com/example/api/view/fragment/PlayerAchievementsFragment.java
@@ -1,13 +1,16 @@
 package com.example.api.view.fragment;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.api.MainViewModel;
 import com.example.api.R;
+import com.example.api.view.SteamListAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +22,15 @@ public class PlayerAchievementsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    MainViewModel mainViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
         return inflater.inflate(R.layout.fragment_player_achievements, container, false);
     }
 
